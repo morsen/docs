340cfc9494008cb478323ab4c63d7fd05024b5e0
Nathan Morse <nathan@eyethereal.com>
2016-03-14 00:53:35 -0700

    Basic working version. The script executes, though udev doesn't work on the Mac, and the script currently isn't actually using Homebrew to install the same/similar packages as apt-get is on Linux. Saving this for the future, for now, and tabling it for later.

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+	sudo brew update && sudo brew upgrade
+
+else
+
+	sudo apt-get update && sudo apt-get -y upgrade
+
+	sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog || \
+	exit_on_error "Failed to install necessary packages."
+
+fi
+
+
 ( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
 sudo easy_install -ZU setuptools && \
 sudo easy_install -ZU openaps && \
@@ -10,6 +23,5 @@ sudo openaps-install-udev-rules && \
 sudo activate-global-python-argcomplete && \
 sudo npm install -g json oref0 && \
 echo openaps installed
+
 openaps --version
-
-
